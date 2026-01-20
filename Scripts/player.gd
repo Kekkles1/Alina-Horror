a7f706c95a94101efd579648ec7451a9dadad03b
Kekkles1 <fatima.mah24314@gmail.com>
2026-01-20 09:47:42 +0500

    added basic first person movement and camera

---
+++ Scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+# meters per second.
+@export var speed = 14
+# meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	# In 3D, the XZ plane is the ground plane.
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.z += 1
+	if Input.is_action_pressed("move_up"):
+		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		$Pivot.basis = Basis.looking_at(direction)
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
